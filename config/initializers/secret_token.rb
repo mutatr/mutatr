9df223c20880125cb2d7eff4e5683812b4a6647a
Jason Meller <jason.meller@gmail.com>
2011-07-21 23:54:03 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mutatr::Application.config.secret_token = 'c1885c7db7fd938fdb1993bba4a84db9898afe8a1b8b40a6e8b33d110147b454741722f63511df9631f88a5898d6d002a70915ebde5822b7d5cd87f78721f24b'
